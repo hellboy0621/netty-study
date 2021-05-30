ceb9d030a05c387e4bfdd4d48e34c3898bf5f7ee
hellboy0621 <hero0621@126.com>
2021-05-30 17:40:12 +0800

    add a new netty server

---
+++ src/main/java/com/xtransformers/netty/chapter01/NettyServer.java
@@ -0,0 +1,42 @@
+package com.xtransformers.netty.chapter01;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author daniel
+ * @date 2021-05-30
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+            // 同步绑定端口
+            ChannelFuture future = serverBootstrap.bind(8080).sync();
+            // 阻塞主线程，直到 Socket 通道被关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/xtransformers/netty/chapter01/ServerHandler.java
@@ -0,0 +1,32 @@
+package com.xtransformers.netty.chapter01;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+
+/**
+ * 注解 @ChannelHandler.Sharable 表示此 Handler 对所有 Channel 共享，无状态，注意多线程并发
+ *
+ * @author daniel
+ * @date 2021-05-30
+ */
+@ChannelHandler.Sharable
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 读取客户端发送的数据
+     *
+     * @param ctx ChannelHandlerContext
+     * @param msg msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof ByteBuf) {
+            System.out.println(((ByteBuf) msg).toString(Charset.defaultCharset()));
+        }
+        ctx.channel().writeAndFlush("msg has recived!");
+    }
+}
